59711a4acd2bf5494f4c35733cb9786dbd79efe7
Simontaga <simon.tornlund@gmail.com>
2020-11-18 17:28:47 +0100

    Change article to use article model instead of raw variables

--- Domain/Models/Article.cs
+++ Domain/Models/Article.cs
@@ -6,5 +6,22 @@ namespace Product_Manager.Domain.Models
 {
     class Article
     {
+
+        public Article(string ArticleNumber,string ArticleName,string Description,int Price) 
+        {
+            this.ArticleNumber = ArticleNumber;
+            this.ArticleName = ArticleName;
+            this.Description = Description;
+            this.Price = Price;
+        }
+
+        public string ArticleNumber { get; }
+
+        public string ArticleName { get; }
+
+        public string Description { get; }
+
+        public int Price { get; }
+
     }
 }
